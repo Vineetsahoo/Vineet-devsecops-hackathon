cf8335c80a61ab2bf231aa7fd18b3a242c015738
Vineetsahoo <vineetsahoo3@gmail.com>
2026-06-05 06:48:56 +0000

    feat: add custom Gitleaks rules and detect secrets

--- challenge1/app.py
+++ challenge1/app.py
@@ -1,6 +1,7 @@
-DB_PASSWORD = "vs1023"
+DB_URL ="postgresql://admin:SuperSecret99@localhost/prod"
+DB_PASSWORD = "vs1023&vineet2005"
 
-AWS_ACCESS_KEY = "AKIAIOSFODNN7EXAMPLE"
+AWS_ACCESS_KEY = "AKIA1234567890ABCDEF""
 
 def connect():
     print("Connecting to database...")
